eb5cc214b07e9a6ead2ef1b15236edc04aa92780
Jordon <jdouglas2015@gmail.com>
2024-08-22 03:40:48 +0000

    adding option 1 storyline

--- NetScape90.sh
+++ NetScape90.sh
@@ -15,14 +15,55 @@ echo "4. Quit"
 
 while true 
 do
-	read playerChoice
-	case $playerChoice in 
-	1) echo "NetScape Loading.......";;
-	2) echo "NetScape Loading.......";;
-	3) echo "NetScape Loading........";;
-	4) echo "Leaving Netscape"
-	  exit;;
-	 *) echo "Invalid choice. Please try again.";;
+    read playerChoice
+    case $playerChoice in 
+        1) 
+            echo "NetScape Loading......."
+            if [ "$playerChoice" -eq 1 ]; then
+                echo "It's Saturday Morning! Time to eat some cereal and watch some cartoons. What channel do you want to watch?"
+		echo "1. Nickeledon"
+		echo "2. Cartoon Network"
+		
+		read channelChoice
+		if [ "$channelChoice" -eq 1 ]; then
+		    echo "Oh No $playerName ! The TV is turning orange! Now there is a blue screen with a message: 'Come Closer' What do we do?"
+		    echo "1. Move Closer"
+	            echo "2. Nah, I'm going to leave now."
+		
+		 read messageChoice
+		 if [ "$messageChoice" -eq 1 ]; then
+	 	     echo "You moved closer! Now you're sucked into the Nickeledon Network! There is a baby with a screwdriver."
+		     echo "1. A baby with a screwdriver? What's the worst that can happen?"
+		     echo "2. Why is there a baby with a screwdriver? Where are some adults?"
+		    
+                    read challenegChoice
+		    if [ "$challenegChoice" -eq 1 ]; then
+		       echo "A Baby's Gotta Do what A Baby's gotta do! Hey! I'm Tommy! You look lost! Are you trying to get back to your mommy and daddy too?"
+
+		       echo  
+		
+		fi 
+            fi
+            ;;
+        2) 
+            echo "NetScape Loading......."
+            if [ "$playerChoice" -eq 2 ]; then
+                echo "It's Wednesday After School!"
+            fi
+            ;;
+        3) 
+            echo "NetScape Loading......."
+            if [ "$playerChoice" -eq 3 ]; then
+                echo "It's Friday Night!"
+            fi
+            ;;
+        4) 
+            echo "Quitting NetScape. Goodbye!"
+            break
+            ;;
+        *) 
+            echo "Invalid choice. Please try again."
+            ;;
     esac
 done
 
